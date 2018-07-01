46eff50d1ffd670db5ff8be7b01dbf9365c097f6
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-01 23:53:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,7 +27,7 @@ class Dog
   end
 
   def self.new_from_db(row)
-    Dog.new(id: row[0], name: row[1], breed: row[2])
+    Dog.new({id: row[0], name: row[1], breed: row[2]})
   end
 
   def self.find_by_name(name)
