fe24e55fdbc33cf93321449f55aa892f0595b904
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-01 23:19:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(name:, breed:, id:)
+  def initialize(name:, breed:, id = nil)
     @id = id
     @name = name
     @breed = breed
