125ece5ed785c37a04bd33d6a77f07ea39c8ec3e
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-01 23:49:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,4 +65,10 @@ class Dog
     end
     dog
   end
+
+  def self.find_by_id(id:)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    result = DB[:conn].execute(sql, id)[0]
+    Dog.new(result[0], result[1], result[2])
+  end
 end
